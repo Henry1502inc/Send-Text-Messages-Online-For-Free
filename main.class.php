09dd9d5b2e6c411a0cfa11e3b4ee70277db77c64
Henry Nwobi <henry1502nc@gmail.com>
2016-03-14 18:15:56 -0400

    Create main.class.php

---
+++ main.class.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+class Send_Sms {
+    public $set_uipn,  // user input phone number
+    public $set_uepn,  // user export phone number
+    public $set_uipnc,  // user input phone number carrier
+    public $set_uepnc,  // user export phone number carrier
+    
+    public function __construct ($set_uipn, $set_uepn, set_uipnc, set_uepnc) {
+        $this->set_uipn = $set_uipn;    
+        $this->set_uepn = $set_uepn;           
+        $this->set_uipnc = $set_uipnc;    
+        $this->set_uepnc = $set_uepnc;           
+    }
+    
+    function get_UIPN () {
+        
+    }
+    
+}
+
+$Send_Sms = new Send_Sms();
+
+
+
+
+
+?>
