d13ffee1195d01a224073007c5e10d6137ed066d
Henry Nwobi <henry1502nc@gmail.com>
2016-03-15 13:22:58 -0400

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="index.js"></script>
+</head>
+
+<body>
+
+
+
+
+
+</body>
+</html>
